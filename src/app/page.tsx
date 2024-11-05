1ba6fcbd711378c66d06e8dc1e4ab34e36235e4a
Lucas Cerveira <lucas.tempass@hotmail.com>
2024-11-04 21:41:04 -0300

    ✨ Cria base do render sem transformações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <section></section>;
+"use client";
+
+import dynamic from "next/dynamic";
+
+const HomeLazy = dynamic(() => import("@/app/home.tsx"), {
+  ssr: false,
+});
+
+export default function Page() {
+  return <HomeLazy />;
 }
