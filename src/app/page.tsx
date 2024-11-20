03131909e636f7f7689b90850d52acc439775410
Lucas Cerveira <lucas.tempass@hotmail.com>
2024-11-20 12:57:44 -0300

    🚧 Adiciona seleção de diretório

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { useState, ChangeEvent } from "react";
+import { useState } from "react";
 
 const CanvasLazy = dynamic(() => import("@/app/_components/Canvas"), {
   ssr: false,
@@ -10,33 +10,90 @@ const CanvasLazy = dynamic(() => import("@/app/_components/Canvas"), {
 export default function Page() {
   const [fileContent, setFileContent] = useState<string | null>(null);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const selectedFile = e.target.files?.[0] ?? null;
+  const [error, setError] = useState<string | null>(null);
 
-    if (!selectedFile) {
+  const [directoryHandle, setDirectoryHandle] =
+    useState<FileSystemDirectoryHandle | null>(null);
+
+  const handleClick = async () => {
+    if (!directoryHandle) return;
+
+    const values = directoryHandle.values();
+
+    const list: FileSystemFileHandle[] = [];
+
+    for await (const value of values) {
+      if (value.kind === "file") {
+        list.push(value);
+      }
+    }
+
+    const indexFile = list.find((v) => v.name === "index.json");
+
+    if (!indexFile) {
+      setError(
+        "Diretório não possui arquivo index.json. Por favor, selecione outro diretório.",
+      );
+
+      setDirectoryHandle(null);
+
       return;
     }
 
-    const reader = new FileReader();
-    reader.onload = (event) => setFileContent(event.target?.result as string);
-    reader.readAsText(selectedFile);
+    const contents = list.map((v) => v.name).join(", ");
+
+    console.log(contents);
   };
 
   if (!fileContent) {
+    const handlePickDir = async () => {
+      setError(null);
+
+      const dirHandle = await window.showDirectoryPicker();
+
+      setDirectoryHandle(dirHandle);
+    };
+
     return (
-      <div className="h-screen w-screen flex flex-col items-center justify-center">
-        <div className="p-6 border-[2px] border-dashed border-white rounded-lg">
-          <h1 className="text-2xl font-medium">
-            Insira um arquivo <span className="font-bold">.obj</span> para
-            visualizar
+      <div className="h-screen w-screen flex flex-col items-center justify-center p-8">
+        <div className="p-6 border-[2px] border-dashed border-white rounded-lg space-y-4">
+          <h1 className="text-2xl font-bold">
+            Selecione um diretório para visualizar a cena
           </h1>
 
-          <input
-            type="file"
-            accept=".obj"
-            className="mt-4 p-2 border-[2px] border-white rounded-lg"
-            onChange={handleChange}
-          />
+          <p className="text-lg">
+            O diretório deve conter um arquivo <code>index.json</code> com a
+            especificação da cena, além de todos os arquivos <code>.obj</code> e{" "}
+            <code>.mtl</code> necessários.
+          </p>
+
+          <button
+            onClick={handlePickDir}
+            className="p-2 bg-yellow-500 text-white rounded-lg"
+          >
+            Selecionar diretório
+          </button>
+
+          {error && (
+            <div className="mt-4 p-2 bg-red-500 text-white rounded-lg">
+              {error}
+            </div>
+          )}
+
+          {directoryHandle && (
+            <div className="space-x-4 flex items-center">
+              <button
+                className="p-2 bg-green-500 text-white rounded-lg"
+                onClick={handleClick}
+              >
+                Visualizar
+              </button>
+
+              <h6 className="font-bold text-lg text-white">
+                Selecionado: {directoryHandle.name}
+              </h6>
+            </div>
+          )}
         </div>
       </div>
     );
